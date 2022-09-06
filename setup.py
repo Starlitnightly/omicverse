960cdb9d43e5ff90fe5130dafb1584bfb6439551
Starlitnightly <41826060@xs.ustb.edu.cn>
2022-09-07 02:36:24 +0800

    add Ipython in requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(  
     name = 'Pyomic',  
-    version = '1.1.0',
+    version = '1.1.1',
     # keywords = ('chinesename',),  
     description = 'A python framework library for omics analysis',  
     license = 'GNU License',  
