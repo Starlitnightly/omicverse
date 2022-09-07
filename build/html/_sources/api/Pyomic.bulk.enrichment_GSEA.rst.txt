96cb192b04998b52edd742ddb79b72381e970cca
Starlitnightly <41826060@xs.ustb.edu.cn>
2022-09-07 13:04:18 +0800

    update to 1.1.2 * add nocd to the module of Pyomic
    * add function `Pyomic.single.scanpy_lazy`
    
    * add class `Pyomic.single.scnocd`
    
    * add tutorial of scnocd in readthedocs
    
    * update Pyomic 1.1.1 -> 1.1.2

--- build/html/_sources/api/Pyomic.bulk.enrichment_GSEA.rst.txt
+++
@@ -1,6 +0,0 @@
-﻿Pyomic.bulk.enrichment\_GSEA
-============================
-
-.. currentmodule:: Pyomic.bulk
-
-.. autofunction:: enrichment_GSEA
--- build/lib/Pyomic/single/__init__.py
+++
@@ -1,7 +0,0 @@
-r"""
-single (A omic framework for single cell omic analysis)
-"""
-
-from ._cosg import cosg
-from ._pySCSA import data_preprocess,cell_annotate,cell_anno_print,scanpy_lazy
-from ._nocd import scnocd
