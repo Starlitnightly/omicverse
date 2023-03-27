b00aa57250a98d8722f46d6dbb26aed404b1fdc2
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-03-28 02:15:39 +0800

    'add module scdrug in single add module TCGA in bulk add module Deseq2 in bulk'

--- build/lib/Pyomic/single/__init__.py
+++ build/lib/Pyomic/single/__init__.py
@@ -6,3 +6,4 @@ from ._cosg import cosg
 from ._pySCSA import data_preprocess,cell_annotate,cell_anno_print,scanpy_lazy
 from ._nocd import scnocd
 from ._mofa import mofa,factor_exact,factor_correlation,get_weights
+from ._scdrug import autoResolution,writeGEP,Drug_Response
