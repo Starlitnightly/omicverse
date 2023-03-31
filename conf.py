dfe4c3e5270581da415bb1d9072322fbe97b4518
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-04-01 00:09:22 +0800

    update conf.py

--- conf.py
+++ conf.py
@@ -73,7 +73,7 @@ from recommonmark.parser import CommonMarkParser
 source_parsers = {
     '.md': CommonMarkParser,
 }
-source_suffix = ['.rst', '.md']
+source_suffix = ['.md']
 
 import inspect
 import sphinx_autodoc_typehints
