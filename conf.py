f937db4046ac70bc942a29710e8f9f9efdedfb61
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-03-31 18:22:34 +0800

    update conf.py

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2022, starlitnightly'
 author = 'starlitnightly'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.7'
 
 
 # -- General configuration ---------------------------------------------------
