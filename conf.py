50b413483f81074d9eedf109dad209e9fbcf465f
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-04-01 00:04:40 +0800

    fix the conf.py

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import Pyomic
+#import Pyomic
 # -- Project information -----------------------------------------------------
 
 project = 'Pyomic'
