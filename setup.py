9caf1f57cdb94ae722f885bbe3e1fba1e15a843d
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-04-13 00:04:43 +0800

    updated to v1.2.7
    ### bulk2single module
    
    - Add `Single2Spatial` module included `load`, `save`, `train`, `spot_assess`
    - Fix the error in install the packages in pip

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ from setuptools import setup, find_packages
 
 setup(  
     name = 'Pyomic',  
-    version = '1.2.6',
+    version = '1.2.7',
     # keywords = ('chinesename',),  
     description = 'A python framework library for omics analysis',  
     license = 'GNU License',  
-    install_requires = ['pybind11','hnswlib','ERgene','numpy','scanpy','pandas','matplotlib','scikit-learn','scipy','networkx','multiprocess',
+    install_requires = ['pybind11','hnswlib','ERgene','numpy','scanpy','pandas==1.5.3','matplotlib','scikit-learn','scipy','networkx','multiprocess',
                         'seaborn','datetime','statsmodels','gseapy==0.10.8','ipywidgets','lifelines','ktplotspy','python-dotplot',
-                        'boltons','ctxcore','termcolor','pygam','pillow','gdown'],  
+                        'boltons','ctxcore','termcolor','pygam','pillow','gdown','igraph','leidenalg','s_gd2','graphtools','datashader',
+                        'phate','wget','deep-forest'],  
     packages = find_packages(),  # 要打包的项目文件夹
     include_package_data=True,   # 自动打包文件夹内所有数据
     author = 'ZehuaZeng',  
