1a01d3ce82eeacdd2975a69d29ce2d452cfbea95
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-05-30 21:21:27 +0800

    update omicverse to 1.4.1
    ### preprocess module
    
    - add `pp`(preprocess) module included `qc`(quantity control), `hvg`(high variable feature), `pca`
    - add `data_files` for cell cycle calculate from [Cellula](https://github.com/andrecossa5/Cellula/) and [pegasus](https://github.com/lilab-bcb/pegasus/)

---
+++ omicverse/pp/__init__.py
@@ -0,0 +1,14 @@
+
+from ._preprocess import (identify_robust_genes,
+                          select_hvf_pegasus,
+                          highly_variable_features,
+                          remove_cc_genes,
+                          preprocess,
+                          normalize_pearson_residuals,
+                          highly_variable_genes,
+                          scale,
+                          regress,
+                          regress_and_scale,
+                          pca)
+
+from ._qc import quantity_control,qc,filter_cells,filter_genes
