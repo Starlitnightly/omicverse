dedd644fabf72e8b1a2852aa357edd7ad2966fdc
Starlitnightly <41826060@xs.ustb.edu.cn>
2023-06-05 17:07:28 +0800

    update omicverse to 1.4.3
    ### preprocess module
    
    - fix sparse preprocess error of `pp`
    - fix the trajectory import error of `via`
    - add the gene correlation analysis of trajectory

--- omicverse/utils/_data.py
+++ omicverse/utils/_data.py
@@ -12,6 +12,10 @@ from ._genomics import read_gtf,Gtf
 import anndata
 import numpy as np
 from typing import Callable, List, Mapping, Optional,Dict
+from ._enum import ModeEnum
+from scipy.sparse import diags, issparse, spmatrix, csr_matrix, isspmatrix_csr
+import warnings
+from scipy.stats import norm
 
 
 def read(path,**kwargs):
@@ -270,3 +274,230 @@ def load_signatures_from_file(input_file: str) -> Dict[str, List[str]]:
             signatures[items[0]] = list(set(items[2:]))
     print(f"Loaded signatures from GMT file {input_file}.")
     return signatures
+
+from typing import (
+    Any,
+    Dict,
+    List,
+    Tuple,
+    Union,
+    Literal,
+    TypeVar,
+    Callable,
+    Hashable,
+    Iterable,
+    Optional,
+    Sequence,
+)
+
+class TestMethod(ModeEnum):  # noqa
+    FISHER = "fisher"
+    PERM_TEST = "perm_test"
+
+
+
+def _mat_mat_corr_sparse(
+    X: csr_matrix,
+    Y: np.ndarray,
+) -> np.ndarray:
+    n = X.shape[1]
+
+    X_bar = np.reshape(np.array(X.mean(axis=1)), (-1, 1))
+    X_std = np.reshape(
+        np.sqrt(np.array(X.power(2).mean(axis=1)) - (X_bar**2)), (-1, 1)
+    )
+
+    y_bar = np.reshape(np.mean(Y, axis=0), (1, -1))
+    y_std = np.reshape(np.std(Y, axis=0), (1, -1))
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        return (X @ Y - (n * X_bar * y_bar)) / ((n - 1) * X_std * y_std)
+
+def correlation_pseudotime(
+    X: Union[np.ndarray, spmatrix],
+    Y: np.ndarray,
+    method: TestMethod = TestMethod.FISHER,
+    n_perms: Optional[int] = None,
+    seed: Optional[int] = None,
+    confidence_level: float = 0.95,
+    **kwargs,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Compute the correlation between rows in matrix ``X`` columns of matrix ``Y``.
+
+    Parameters
+    ----------
+    X
+        Array or matrix of `(M, N)` elements.
+    Y
+        Array of `(N, K)` elements.
+    method
+        Method for p-value calculation.
+    n_perms
+        Number of permutations if ``method='perm_test'``.
+    seed
+        Random seed if ``method = 'perm_test'``.
+    confidence_level
+        Confidence level for the confidence interval calculation. Must be in `[0, 1]`.
+    kwargs
+        Keyword arguments for :func:`cellrank._utils._parallelize.parallelize`.
+
+    Returns
+    -------
+        Correlations, p-values, corrected p-values, lower and upper bound of 95% confidence interval.
+        Each array if of shape ``(n_genes, n_lineages)``.
+    """
+
+    def perm_test_extractor(
+        res: Sequence[Tuple[np.ndarray, np.ndarray]]
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        pvals, corr_bs = zip(*res)
+        pvals = np.sum(pvals, axis=0) / float(n_perms)
+
+        corr_bs = np.concatenate(corr_bs, axis=0)
+        corr_ci_low, corr_ci_high = np.quantile(corr_bs, q=ql, axis=0), np.quantile(
+            corr_bs, q=qh, axis=0
+        )
+
+        return pvals, corr_ci_low, corr_ci_high
+
+    if not (0 <= confidence_level <= 1):
+        raise ValueError(
+            f"Expected `confidence_level` to be in interval `[0, 1]`, found `{confidence_level}`."
+        )
+
+    n = X.shape[1]  # genes x cells
+    ql = 1 - confidence_level - (1 - confidence_level) / 2.0
+    qh = confidence_level + (1 - confidence_level) / 2.0
+
+    if issparse(X) and not isspmatrix_csr(X):
+        X = csr_matrix(X)
+
+    corr = _mat_mat_corr_sparse(X, Y) if issparse(X) else _mat_mat_corr_dense(X, Y)
+
+    if method == TestMethod.FISHER:
+        # see: https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#Using_the_Fisher_transformation
+        mean, se = np.arctanh(corr), 1.0 / np.sqrt(n - 3)
+        z_score = (np.arctanh(corr) - np.arctanh(0)) * np.sqrt(n - 3)
+
+        z = norm.ppf(qh)
+        corr_ci_low = np.tanh(mean - z * se)
+        corr_ci_high = np.tanh(mean + z * se)
+        pvals = 2 * norm.cdf(-np.abs(z_score))
+    else:
+        raise NotImplementedError(method)
+    '''
+    elif method == TestMethod.PERM_TEST:
+        if not isinstance(n_perms, int):
+            raise TypeError(
+                f"Expected `n_perms` to be an integer, found `{type(n_perms).__name__}`."
+            )
+        if n_perms <= 0:
+            raise ValueError(f"Expcted `n_perms` to be positive, found `{n_perms}`.")
+
+
+        pvals, corr_ci_low, corr_ci_high = parallelize(
+            _perm_test,
+            np.arange(n_perms),
+            as_array=False,
+            unit="permutation",
+            extractor=perm_test_extractor,
+            **kwargs,
+        )(corr, X, Y, seed=seed)
+    '''
+    
+
+    return corr, pvals, corr_ci_low, corr_ci_high
+
+def _np_apply_along_axis(func1d, axis: int, arr: np.ndarray) -> np.ndarray:
+    """
+    Apply a reduction function over a given axis.
+
+    Parameters
+    ----------
+    func1d
+        Reduction function that operates only on 1 dimension.
+    axis
+        Axis over which to apply the reduction.
+    arr
+        The array to be reduced.
+
+    Returns
+    -------
+    The reduced array.
+    """
+
+    assert arr.ndim == 2
+    assert axis in [0, 1]
+
+    if axis == 0:
+        result = np.empty(arr.shape[1])
+        for i in range(len(result)):
+            result[i] = func1d(arr[:, i])
+        return result
+
+    result = np.empty(arr.shape[0])
+    for i in range(len(result)):
+        result[i] = func1d(arr[i, :])
+
+    return result
+
+def np_mean(array: np.ndarray, axis: int) -> np.ndarray:  # noqa
+    return _np_apply_along_axis(np.mean, axis, array)
+
+def np_std(array: np.ndarray, axis: int) -> np.ndarray:  # noqa
+    return _np_apply_along_axis(np.std, axis, array)
+
+def _mat_mat_corr_dense(X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    #from cellrank.kernels._utils import np_std, np_mean
+
+    n = X.shape[1]
+
+    X_bar = np.reshape(np_mean(X, axis=1), (-1, 1))
+    X_std = np.reshape(np_std(X, axis=1), (-1, 1))
+
+    y_bar = np.reshape(np_mean(Y, axis=0), (1, -1))
+    y_std = np.reshape(np_std(Y, axis=0), (1, -1))
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", RuntimeWarning)
+        return (X @ Y - (n * X_bar * y_bar)) / ((n - 1) * X_std * y_std)
+
+
+def _perm_test(
+    ixs: np.ndarray,
+    corr: np.ndarray,
+    X: Union[np.ndarray, spmatrix],
+    Y: np.ndarray,
+    seed: Optional[int] = None,
+    queue=None,
+) -> Tuple[np.ndarray, np.ndarray]:
+    rs = np.random.RandomState(None if seed is None else seed + ixs[0])
+    cell_ixs = np.arange(X.shape[1])
+    pvals = np.zeros_like(corr, dtype=np.float64)
+    corr_bs = np.zeros((len(ixs), X.shape[0], Y.shape[1]))  # perms x genes x lineages
+
+    mmc = _mat_mat_corr_sparse if issparse(X) else _mat_mat_corr_dense
+
+    for i, _ in enumerate(ixs):
+        rs.shuffle(cell_ixs)
+        corr_i = mmc(X, Y[cell_ixs, :])
+        pvals += np.abs(corr_i) >= np.abs(corr)
+
+        bootstrap_ixs = rs.choice(cell_ixs, replace=True, size=len(cell_ixs))
+        corr_bs[i, :, :] = mmc(X[:, bootstrap_ixs], Y[bootstrap_ixs, :])
+
+        if queue is not None:
+            queue.put(1)
+
+    if queue is not None:
+        queue.put(None)
+
+    return pvals, corr_bs
+
+def anndata_sparse(adata):
+    from scipy.sparse import csr_matrix
+    x = csr_matrix(adata.X.copy())
+    adata.X=x
+    return adata
