c41a7650f876839cb755393a13ac76e1065e3ce0
Starlitnightly <starlitnightly@163.com>
2024-06-28 14:37:32 +0800

    Move module `tape`,`SEACells` and `palantir` to `externel`

--- omicverse/externel/__init__.py
+++ omicverse/externel/__init__.py
@@ -1,1 +1,3 @@
-from . import scSLAT,CEFCON,mofapy2,GNTD,spaceflow,STT,tosica,STAGATE_pyG,STAligner,spatrio,PROST,cytotrace2
+from . import (scSLAT,CEFCON,mofapy2,GNTD,spaceflow,STT,
+               tosica,STAGATE_pyG,STAligner,spatrio,PROST,cytotrace2,
+               )
