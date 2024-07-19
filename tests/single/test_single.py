0b391f41431d03ca64c04305949fcbe3e8e24afd
Starlitnightly <starlitnightly@163.com>
2024-07-20 03:23:39 +0800

    Added `enrichment_multi_concat` in `ov.bulk` to concat the result of enrichment.

--- tests/single/test_single.py
+++ tests/single/test_single.py
@@ -13,28 +13,3 @@ def test_pp():
     #ov.pp.pca(adata,layer='scaled',n_pcs=50)
     assert adata.layers['scaled'] is not None
 
-
-def test_anno():
-    adata=ov.utils.pancreas()
-    adata=ov.single.scanpy_lazy(adata)
-    scsa=ov.single.pySCSA(adata=adata,
-                          foldchange=1.5,
-                          pvalue=0.01,
-                          celltype='normal',
-                          target='cellmarker',
-                          tissue='All',
-    )
-    anno=scsa.cell_anno(clustertype='leiden',
-               cluster='all',rank_rep=True)
-    assert anno is not None
-
-def test_metatime():
-    adata=ov.utils.pancreas()
-    adata=ov.single.scanpy_lazy(adata)
-    TiME_object=ov.single.MetaTiME(adata,mode='table')
-    TiME_object.overcluster(resolution=8,clustercol = 'overcluster',)
-    TiME_object.predictTiME(save_obs_name='MetaTiME')
-    assert TiME_object.adata.obs['MetaTiME'].shape[0]==adata.shape[0]
-
-
-
