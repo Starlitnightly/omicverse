4abf95f0282f7fe0cc3b27dd78ee0a6d1b319978
HendricksJudy <61645034+HendricksJudy@users.noreply.github.com>
2024-12-14 22:22:46 -0800

    Update t_via_annotated.py

--- OvStudent/Converted_Scripts_Annotated/t_via_annotated.py
+++ OvStudent/Converted_Scripts_Annotated/t_via_annotated.py
@@ -1,4 +1,4 @@
-```python
+```
 # Line 1:  Import the omicverse library as ov. -- import omicverse as ov
 # Line 2:  Import the scanpy library as sc. -- import scanpy as sc
 # Line 3:  Import the matplotlib.pyplot library as plt. -- import matplotlib.pyplot as plt
