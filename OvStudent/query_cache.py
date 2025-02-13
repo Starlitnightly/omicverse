a081e349ba59caadf93633ca6b7e27536aa58069
HendricksJudy <61645034+HendricksJudy@users.noreply.github.com>
2025-02-12 20:45:45 -0800

    Delete OvStudent directory

--- OvStudent/query_cache.py
+++
@@ -1,9 +0,0 @@
-# query_cache.py
-
-from collections import OrderedDict
-from ttl_cache import TTLCache
-import os
-
-class QueryCache(TTLCache):
-    def __init__(self, maxsize=1000, ttl=3600):
-        super().__init__(maxsize=maxsize, ttl=ttl)
