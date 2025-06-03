2530fc0cb0bd905b425b9c45a9605fe7efb6a0bd
HendricksJudy <61645034+HendricksJudy@users.noreply.github.com>
2025-06-04 00:26:16 +0300

    Simplify imports and add lightweight preprocessing stubs

--- omicverse/bulk/__init__.py
+++ omicverse/bulk/__init__.py
@@ -1,14 +1,5 @@
-r"""
-bulk (A omic framework for bulk omic analysis)
-"""
+"""Bulk omics analysis utilities."""
 
-#from Pyomic.bulk.DeGene import find_DEG,Density_norm,Plot_gene_expression,ID_mapping
-#from Pyomic.bulk.Gene_module import pywgcna
+# Heavy functionality lives in submodules and is imported lazily.
 
-from ._Gene_module import pyWGCNA,readWGCNA
-from ._Enrichment import pyGSEA,pyGSE,geneset_enrichment,geneset_plot,geneset_enrichment_GSEA,geneset_plot_multi,enrichment_multi_concat
-from ._network import pyPPI,string_interaction,string_map,generate_G
-from ._chm13 import get_chm13_gene,find_chm13_gene
-from ._Deseq2 import pyDEG,deseq2_normalize,estimateSizeFactors,estimateDispersions,Matrix_ID_mapping,data_drop_duplicates_index
-from ._tcga import pyTCGA
-from ._combat import batch_correction
+__all__ = []
