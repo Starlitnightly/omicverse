9eca375ef179b8e550d437ecc33245deff689065
Starlitnightly <starlitnightly@163.com>
2025-06-04 06:54:20 -0700

    Fixed the error of docs construction

--- omicverse/externel/__init__.py
+++ omicverse/externel/__init__.py
@@ -1,3 +1,34 @@
 """Collection of optional external algorithms."""
-
-__all__ = []
+from . import (scSLAT,CEFCON,mofapy2,GNTD,spaceflow,STT,
+               tosica,STAGATE_pyG,STAligner,spatrio,PROST,cytotrace2,
+               GraphST,commot,cnmf,starfysh,scMulan,flowsig,PyWGCNA,
+               CAST,scanorama,scdiffusion,BINARY,cellanova,VIA,gaston
+               )
+__all__ = [
+    'scSLAT',
+    'CEFCON',
+    'mofapy2',
+    'GNTD',
+    'spaceflow',
+    'STT',
+    'tosica',
+    'STAGATE_pyG',
+    'STAligner',
+    'spatrio',
+    'PROST',
+    'cytotrace2',
+    'GraphST',
+    'commot',
+    'cnmf',
+    'starfysh',
+    'scMulan',
+    'flowsig',
+    'PyWGCNA',
+    'CAST',
+    'scanorama',
+    'scdiffusion',
+    'BINARY',
+    'cellanova',
+    'VIA',
+    'gaston',
+]
--- omicverse/pl/__init__.py
+++ omicverse/pl/__init__.py
@@ -1,3 +1,13 @@
 """Plotting utilities."""
+from ._palette import *
+from ._single import *
+from ._general import *
+from ._heatmap import *
+from ._multi import *
+from ._bulk import *
+from ._space import *
+from ._cpdb import *
+from ._flowsig import *
+from ._embedding import *
+from ._density import *
 
-__all__ = []
