6ade83ed1ea82047999207c557bc7cfb505b79d1
Starlitnightly <starlitnightly@163.com>
2025-07-20 17:36:13 -0700

    Optimizate the arrow and svg save in `embedding`

--- omicverse/pl/_single.py
+++ omicverse/pl/_single.py
@@ -1409,3 +1409,36 @@ def dotplot_doublegroup(adata, gene, group1, group2, cmap='Reds',
     ax1.grid(False)
     ax1.axis(False)
     plt.show()
+
+
+def add_arrow(ax,adata,basis,fontsize=12,
+              x_label=None,y_label=None):
+    r"""
+    Add arrow and label to the axis
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axis to add the arrow and label to.
+    adata : AnnData
+        The AnnData object to add the arrow and label to.
+    """
+    if x_label is None:
+        x_label=basis+'1'
+    if y_label is None:
+        y_label=basis+'2'
+    
+    x_range=(adata.obsm[basis][:,0].max()-adata.obsm[basis][:,0].min())/6
+    y_range=(adata.obsm[basis][:,1].max()-adata.obsm[basis][:,1].min())/6
+    x_min=adata.obsm[basis][:,0].min()
+    y_min=adata.obsm[basis][:,1].min()
+    ax.arrow(x=x_min-x_range/5,y=y_min,dx=x_range+x_range/5,dy=0, 
+            width=0.01, color="k", 
+                head_width=y_range*2/5, head_length=x_range*2/5, overhang=0.5)
+
+    ax.arrow(x=x_min,y=y_min-y_range/5,dx=0,dy=y_range+y_range/5, 
+            width=0.01, color="k", 
+                head_width=x_range*2/5, head_length=y_range*2/5, overhang=0.5)
+    ax.text(x=x_min,y=y_min-y_range/2,s=x_label,fontsize=fontsize,multialignment='center',
+            verticalalignment='center')
+    ax.text(x=x_min-x_range/2,y=y_min,s=y_label,fontsize=fontsize,rotation='vertical',multialignment='center',
+           horizontalalignment='center')
